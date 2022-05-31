1570fb8878975ee05050e4c4e8bdcc5036799a16
ArkHiMed-tech <74063313+ArkHiMed-tech@users.noreply.github.com>
2022-05-31 13:47:21 +0200

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,4 +1,3 @@
-import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -10,32 +9,39 @@ class Bomber:
         self.url = self.link.replace('viewform', '').replace('formResponse', '') + 'formResponse'
         self.mail = mail
 
-    def start(self, num_of_requests, *answers, question_seq=None, print_result=False):
-        '''Start bombing'''
+    def start(self, num_of_requests:int, answers:list, question_seq:list[int]=None, print_result=False):
+        '''Start bombing\n
+           `answers` :list[any]:\n
+           `question_seq` :list[int] | int: is sequence of questions to answer.'''
         def sorting(x):
             x = x['data-params'][x['data-params'].index(',[[') + 3:]
             return x[:x.index(',')]
-        
         req_list = []
-        response = requests.get(self.link)
+        try:
+            response = requests.get(self.link)
+        except requests.exceptions.ConnectionError as error:
+            print(f'You have some troubles with internet connection, try later\n{error}')
+            return error
         soup = BeautifulSoup(response.text, 'lxml')
         params = list(map(sorting, soup.find_all('div', jsmodel = "CP1oW")))
-        print(params)
         for i in range(num_of_requests):
-            form_data = {'entry.986720168': ''.join(random.choices(list('1234567890'), k=1)),
-                        'entry.1704626263': random.sample(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'], k=random.randint(1, 10)),
-                        'entry.1679341224': random.choice([1, 2, 3, 4]),
+            form_data = {
                         'draftResponse':[],
                         'pageHistory':0}
+            if question_seq is not None:
+                for j, v in enumerate(question_seq):
+                    form_data['entry.' + params[v]] = answers[j]
+            else:
+                for j, v in enumerate(params):
+                    form_data['entry.' + v] = answers[j]
             user_agent = {'Referer': self.link,'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.52 Safari/537.36"}
             r = requests.post(self.url, data=form_data, headers=user_agent)
             req_list.append(r.status_code)
             if print_result:
                 print(f'{i + 1} / {num_of_requests} - {"Sucsessful" if r.status_code == 200 else f"Failed: {r.status_code}"}')
         return req_list
-        
-            
+
 
 if __name__ == '__main__':
     bber = Bomber('https://docs.google.com/forms/d/e/1FAIpQLSe9Nf5RATGkcd01JAjWRVqMY0dqrhOMoODewsd6E16p5d3r9g/viewform', 'boss.trushchelev@mail.ru')
-    bber.start(0, 1, 56, print_result=True)
+    bber.start(10, ['answer1', [2, 4], 3], print_result=True)
